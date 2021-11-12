1c3b34b3468c21df72b5538d1cc151f87c6bbab5
鶴久ゆきな <tsuruhisayukina@tsurukuyukinanoMacBook-Pro.local>
2021-11-12 18:23:10 +0900

    3Dgraph表示成功

---
+++ opt/3D_graph.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x1, x2):
+  return x1**2 + x2**2
+
+x1 = np.arange(-5.0, 5.0, 0.1)
+x2 = np.arange(-5.0, 5.0, 0.1)
+
+X1, X2 = np.meshgrid(x1, x2)
+F = f(X1, X2)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+ax.plot_wireframe(X1, X2, F)
+
+plt.savefig('3Dgraph.jpg')
